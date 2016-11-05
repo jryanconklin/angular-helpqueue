24269cecc2a7002fbbdaafb220ad4fbc4c791105
J. Ryan Conklin <jryanconklin@gmail.com>
2016-11-05 11:58:20 -0700

    Add AngularFire2 and Config to Halp Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+//Firebase Config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDy3keUHNQdyxi0CEs-jkY51xbUIb99K8I',
+  authDomain: 'halp-e491e.firebaseapp.com',
+  databaseURL: 'https://halp-e491e.firebaseio.com',
+  storageBucket: 'halp-e491e.appspot.com'
+};
 
 @NgModule({
   declarations: [
